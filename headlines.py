8335121a8f0273fadcda97165ea3e4b1578b0fb7
max777888 <issamnader@gmail.com>
2018-04-13 15:53:28 +0300

    adding dynamic content for the template call

--- headlines.py
+++ headlines.py
@@ -20,7 +20,10 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
   feed = feedparser.parse(RSS_FEEDS[publication])
   first_article = feed['entries'][0]
-  return render_template("home.html")
+  render_template("home.html",
+          title=first_article.get("title"),
+          published=first_article.get("published"),
+          summary=first_article.get("summary"))
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', threaded=True, port=5000)
